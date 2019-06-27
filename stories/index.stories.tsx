22c9f7db9036c112d6a0a1eb9fac08359039c9b3
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-06-27 15:27:41 +0300

    Center components in storybook

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,8 +1,15 @@
+import { Grid } from '@material-ui/core';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { BarChart } from '../src';
 
+// tslint:disable-next-line: variable-name
+const CenterDecorator = (storyFn: any) => (
+  <Grid container justify="center" alignItems="center">
+    {storyFn()}
+  </Grid>
+);
+
 storiesOf('Hurumap UI|Charts/BarChart', module)
-  .add('Default', () => (
-    <BarChart/>
-  ));
+  .addDecorator(CenterDecorator)
+  .add('Default', () => <BarChart />);
