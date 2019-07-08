f2819deb81507cb229b36e828c7ad840f0740a88
Clemence Kyara <clemence@codeforafrica.org>
2019-07-08 22:03:30 +0300

    Improve proportions calculation

--- src/NestedProportionalAreaChart/proportional.ts
+++ src/NestedProportionalAreaChart/proportional.ts
@@ -4,9 +4,8 @@ export default function calculateProportions(
   data: number[],
   size: number = DEFAULT_SIZE
 ): number[] {
-  const [total, data1, data2] = data;
-  const r1 = (data1 * size) / total;
-  const r2 = data2 ? (data2 * size) / total : 0;
-
-  return [size, r1, r2];
+  const [total] = data;
+  return data.map((value, index) =>
+    index === 0 ? size : (value * size) / total
+  );
 }
