7d2438e1a83692ed87a90ac274e8b545630284ce
Clemence Kyara <clemence@codeforafrica.org>
2019-07-10 16:36:25 +0300

    Implement basic single and group of line charts

--- src/LineChart.tsx
+++ src/LineChart.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
-import { Theme, WithStyles } from '@material-ui/core';
-import { createStyles, useTheme, withStyles } from '@material-ui/styles';
-import { VictoryLine, VictoryLineProps } from 'victory';
+import { Theme } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
+import {
+  VictoryLine,
+  VictoryChartProps,
+  VictoryChart,
+  VictoryGroup,
+  VictoryTooltip,
+  VictoryScatter,
+  VictoryVoronoiContainer
+} from 'victory';
 
 import ThemedComponent from './ThemedComponent';
 
-const styles = createStyles({
-  root: {}
-});
-
-interface Props extends WithStyles<typeof styles>, VictoryLineProps {}
-
-function LineChart({ ...props }: Props) {
-  const theme = useTheme<Theme>();
-  return <VictoryLine theme={theme.chart} {...props} />;
+interface Props extends VictoryChartProps {
+  data: any[];
 }
 
-export default withStyles(styles)(({ ...props }: Props) => {
+function LineChart({ data, ...props }: Props) {
+  const { chart: theme } = useTheme<Theme>();
+  if (!data || !theme) {
+    return null;
+  }
+
+  const [mainData, extraData] =
+    data.length > 1 && Array.isArray(data[0])
+      ? [data[0], data.slice(1)]
+      : [data, []];
+  return (
+    <VictoryChart containerComponent={<VictoryVoronoiContainer />} {...props}>
+      <VictoryGroup
+        labels={d => `${d.y}`}
+        labelComponent={<VictoryTooltip />}
+        data={mainData}
+      >
+        <VictoryLine />
+        <VictoryScatter />
+      </VictoryGroup>
+      {extraData.map(xd => (
+        <VictoryGroup
+          data={xd}
+          labels={d => `${d.y}`}
+          labelComponent={<VictoryTooltip />}
+        >
+          <VictoryLine />
+          <VictoryScatter />
+        </VictoryGroup>
+      ))}
+    </VictoryChart>
+  );
+}
+
+export default function({ ...props }: Props) {
   return (
     <ThemedComponent>
       <LineChart {...props} />
     </ThemedComponent>
   );
-});
+}
