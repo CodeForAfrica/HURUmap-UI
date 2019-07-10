5d7200078b8813916e3a53e0e2fcd532181bb98c
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-07-10 09:53:02 +0300

    demo charts

--- example/src/Profile.tsx
+++ example/src/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { MapIt } from 'hurumap-ui';
+import { MapIt, PieChart, GroupedBarChart } from 'hurumap-ui';
 import { RouteComponentProps } from '@reach/router';
 
 import bg from './assets/images/background.png';
@@ -25,81 +25,154 @@ function Profile({ geoId, navigate }: Props) {
   console.log(bg);
   return (
     <div
-      style={{ width: '100vw', height: '100vh', backgroundImage: `url(${bg})`, overflowX: 'hidden' }}
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${bg})`,
+        overflowX: 'hidden'
+      }}
     >
-        <ProfileHero
-          geoCode={geoCode}
-          geoLevel={geoLevel}
-          profile={{
-            demographics: {
-              total_population: {
-                values: {
-                  this: 9999
-                }
-              },
-              population_density: {
-                values: {
-                  this: 999999.9
-                }
+      <ProfileHero
+        geoCode={geoCode}
+        geoLevel={geoLevel}
+        profile={{
+          demographics: {
+            total_population: {
+              values: {
+                this: 9999
               }
             },
-            primary_releases: {
-              active: {
-                citation: 'citation'
-              }
-            },
-            geography: {
-              parents: { example: { name: 'Demo', full_geoid: 'Demo' } },
-              this: {
-                square_kms: '6666.6',
-                short_name: 'Demo'
+            population_density: {
+              values: {
+                this: 999999.9
               }
             }
+          },
+          primary_releases: {
+            active: {
+              citation: 'citation'
+            }
+          },
+          geography: {
+            parents: { example: { name: 'Demo', full_geoid: 'Demo' } },
+            this: {
+              square_kms: '6666.6',
+              short_name: 'Demo'
+            }
+          }
+        }}
+      >
+        <MapIt
+          drawChildren
+          drawProfile={Boolean(geoId)}
+          codeType="AFR"
+          geoLevel={geoLevel}
+          geoCode={geoCode}
+          filterCountries={['KE', 'TZ', 'ZA']}
+          onClickGeoLayer={area => {
+            if (navigate) {
+              navigate(`/${area.codes['AFR']}`);
+            }
           }}
-        >
-          <MapIt
-            drawChildren
-            drawProfile={Boolean(geoId)}
-            codeType="AFR"
-            geoLevel={geoLevel}
-            geoCode={geoCode}
-            filterCountries={['KE', 'TZ', 'ZA']}
-            onClickGeoLayer={area => {
-              if (navigate) {
-                navigate(`/${area.codes['AFR']}`);
-              }
-            }}
-          />
-        </ProfileHero>
-        <ProfileTabs tabs={[{ name: 'Tab #1', href: '#1' }, { name: 'Tab #2', href: '#2' },{ name: 'Tab #3', href: '#3' }]} />
-        <ChartsContainer>
-          <Grid container spacing={2}>
-
+        />
+      </ProfileHero>
+      <ProfileTabs
+        tabs={[
+          { name: 'Tab #1', href: '#1' },
+          { name: 'Tab #2', href: '#2' },
+          { name: 'Tab #3', href: '#3' }
+        ]}
+      />
+      <ChartsContainer>
+        <Grid container spacing={2}>
           <Grid item md={8}>
-          <ChartContainer title="Population" subtitle="People living there">
-
-          </ChartContainer>
+            <ChartContainer title="Population" subtitle="People living there">
+              <GroupedBarChart
+              width="100%"
+              height="200px"
+              dataUnit="%"
+                data={[
+                  {
+                    x: 'Slept under any net last night',
+                    data: [
+                      { x: 'Male', y: 49 },
+                      { x: 'Female', y: 51 }
+                    ]
+                  }
+                ]}
+              />
+            </ChartContainer>
           </Grid>
           <Grid item md={4}>
-          <ChartContainer title="Extension" subtitle="Surface in km2">
-
-          </ChartContainer>
+            <ChartContainer title="Extension" subtitle="Surface in km2">
+              <GroupedBarChart
+              width="100%"
+              height="200px"
+              horizontal
+              dataUnit="%"
+                data={[
+                  {
+                    x: 'Slept under any net last night',
+                    data: [
+                      { x: '17', y: 50 },
+                      { x: '25', y: 40 }
+                    ]
+                  }
+                ]}
+              />
+            </ChartContainer>
           </Grid>
-          </Grid>
-          <Grid container spacing={2}>
-
+        </Grid>
+        <Grid container spacing={2}>
           <Grid item md={8}>
-          <ChartContainer title="Age distribution" subtitle="">
-
-          </ChartContainer>
+            <ChartContainer title="Age distribution" subtitle="">
+              <GroupedBarChart
+              width="100%"
+              height="200px"
+              dataUnit="%"
+                data={[
+                  {
+                    x: 'Slept under any net last night',
+                    data: [
+                      { x: '0-9', y: 40 },
+                      { x: '10-19', y: 20 },
+                      { x: '20-29', y: 10 },
+                      { x: '30-39', y: 5 },
+                      { x: '40-49', y: 5 },
+                      { x: '50-59', y: 10 },
+                      { x: '60-69', y: 10 },
+                      { x: '70-79', y: 0 },
+                      { x: '80+', y: 0 }
+                    ]
+                  }
+                ]}
+              />
+            </ChartContainer>
           </Grid>
           <Grid item md={4}>
-          <ChartContainer title="Median age" subtitle="Average age of the population (years old)">
-
-          </ChartContainer>
+            <ChartContainer
+              title="Median age"
+              subtitle="Average age of the population (years old)"
+            >
+              <GroupedBarChart
+              width="100%"
+              height="200px"
+              horizontal
+              dataUnit="%"
+                data={[
+                  {
+                    x: 'Slept under any net last night',
+                    data: [
+                      { x: '17', y: 50 },
+                      { x: '25', y: 40 }
+                    ]
+                  }
+                ]}
+              />
+            </ChartContainer>
           </Grid>
-          </Grid>
-        </ChartsContainer>
+        </Grid>
+      </ChartsContainer>
     </div>
   );
 }
