a7c10a805e3e901ff0ee7055eb565d4768b86d84
Clemence Kyara <clemence@codeforafrica.org>
2019-07-10 16:43:26 +0300

    Small stylistic fixes

--- src/LineChart.tsx
+++ src/LineChart.tsx
@@ -14,6 +14,7 @@ import {
 import ThemedComponent from './ThemedComponent';
 
 interface Props extends VictoryChartProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any[];
 }
 
