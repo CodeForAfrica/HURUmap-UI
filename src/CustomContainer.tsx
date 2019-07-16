ee2162187b7dd6a42fa6f2c21ff58540e8df3a65
Clemence Kyara <clemence@codeforafrica.org>
2019-07-16 16:41:52 +0300

    Add custom container similar to VictoryContainer

---
+++ src/CustomContainer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export interface CustomContainerProps {
+  standalone?: boolean;
+  height?: number;
+  responsive?: boolean;
+  style?: React.CSSProperties;
+  width?: number;
+  children?: any;
+}
+
+/**
+ * Simple custom container similar to VictoryContainer that should be used with
+ * components that don't work with VictoryChart i.e. those that don't need
+ * Cartesian or polar axes.
+ */
+function CustomContainer({
+  children,
+  height,
+  responsive = true,
+  standalone = true,
+  style,
+  width
+}: CustomContainerProps) {
+  if (!standalone) {
+    return children;
+  }
+
+  const dimensions = responsive
+    ? { width: '100%', height: 'auto' }
+    : { width, height };
+  const divStyle = Object.assign(
+    { pointerEvents: 'none', touchAction: 'none', position: 'relative' },
+    dimensions
+  );
+  const svgProps = Object.assign({
+    width,
+    height,
+    role: 'img',
+    viewBox: responsive ? `0 0 ${width} ${height}` : undefined
+  });
+  const svgStyle = Object.assign({ pointerEvents: 'all' }, dimensions);
+
+  return (
+    <div style={Object.assign({}, style, divStyle)}>
+      <svg {...svgProps} style={svgStyle}>
+        {children}
+      </svg>
+    </div>
+  );
+}
+
+export default CustomContainer;
