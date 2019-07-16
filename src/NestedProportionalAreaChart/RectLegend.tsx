de2807109b33069124b678a36fc64d5464cbedf8
nerdlet <brndnyokabi@gmail.com>
2019-07-16 11:59:51 +0300

    Update RectLegend

--- src/NestedProportionalAreaChart/RectLegend.tsx
+++ src/NestedProportionalAreaChart/RectLegend.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
+import { WithStyles } from '@material-ui/core';
+import { createStyles, withStyles } from '@material-ui/styles';
+import ThemedComponent from '../ThemedComponent';
 
-function RectLegend() {
+const styles = createStyles({
+  legendRoot: {
+    height: '4rem',
+    width: '5rem'
+  },
+  topLegend: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    left: '3rem',
+    fontSize: '2.5rem',
+    color: 'grey',
+    listStyleType: 'none'
+  },
+  bottomLegend: {
+    position: 'absolute',
+    zIndex: 1,
+    top: '32.5rem',
+    left: '3rem',
+    fontSize: '1rem'
+  }
+});
+
+interface Props extends WithStyles<typeof styles> {
+  classes: any;
+}
+
+function RectLegend({ classes }: Props) {
   return (
     <div>
-      <div style={{ height: '4rem', width: '5rem' }}>
-        <g
-          style={{
-            position: 'absolute',
-            zIndex: 1,
-            top: 0,
-            left: '3rem',
-            fontSize: '3rem',
-            color: 'grey',
-            listStyleType: 'none'
-          }}
-        >
+      <div className={classes.legendRoot}>
+        <g className={classes.topLegend}>
           <li>
-            <text style={{ color: 'green' }}>760,000</text>
+            <text style={{ color: '#7f9442' }}>760,000</text>
           </li>
           <li>
-            <text style={{ color: 'orange' }}>679,000</text>
+            <text style={{ color: '#de9f3a' }}>679,000</text>
           </li>
         </g>
       </div>
 
-      <g
-        style={{
-          position: 'absolute',
-          zIndex: 1,
-          top: '32.5rem',
-          left: '3rem',
-          fontSize: '1rem',
-          color: 'grey'
-        }}
-      >
+      <g className={classes.bottomLegend}>
         <text>
           47.9m
           <br />
@@ -44,4 +55,10 @@ function RectLegend() {
   );
 }
 
-export default RectLegend;
+export default withStyles(styles)(({ ...props }: Props) => {
+  return (
+    <ThemedComponent>
+      <RectLegend {...props} />
+    </ThemedComponent>
+  );
+});
