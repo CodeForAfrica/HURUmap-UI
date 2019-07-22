cb3f9e916d39798096a5ce652a509acf7c8ade93
Clemence Kyara <clemence@codeforafrica.org>
2019-07-22 11:50:50 +0300

    Initial legend support for mobile screens i.e. vertical

--- src/NestedProportionalAreaChart/index.tsx
+++ src/NestedProportionalAreaChart/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   VictoryCommonProps,
   VictoryDatableProps,
+  VictoryLabel,
   VictoryMultiLabeableProps,
   VictoryThemeDefinitionLatest
 } from 'victory';
@@ -52,13 +53,26 @@ function NestedProportionalAreaChart({
   const computedWidth = width || chart.width;
   const computedGroupSpacing =
     data.length > 1 ? groupSpacing || chart.groupSpacing : 0;
-  const minDimension = Math.min(computedHeight, computedWidth);
+
+  // For starters, lets assume each data label has 36px height,
+  // reference label has 48 px, and there is 10px between labels
+  // and charts
+  // ------------------------------------
+
+  const chartHeight = computedHeight - (data.length * 36 + 48 + 20);
+  const minDimension = Math.min(chartHeight, computedWidth);
+  const dataLabelStyles = (index: number): React.CSSProperties => ({
+    fontSize: 36,
+    fontWeight: 'bold',
+    fill: chart.colorScale[index % chart.colorScale.length]
+  });
+  const referenceLabelStyles = (index: number): React.CSSProperties =>
+    Object.assign({}, reference.style.labels, {
+      fontWeight: index === 0 ? 'bold' : 'normal'
+    }) as React.CSSProperties;
 
   return (
-    <CustomContainer
-      height={height || chart.height}
-      width={width || chart.height}
-    >
+    <CustomContainer height={computedHeight} width={computedWidth}>
       <defs>
         <pattern
           id="gradient-background"
@@ -71,30 +85,60 @@ function NestedProportionalAreaChart({
         </pattern>
       </defs>
 
+      <g>
+        {data.map((d, i) => (
+          <VictoryLabel
+            capHeight={0}
+            lineHeight={0}
+            x={(computedWidth - minDimension) / 2}
+            dx={0}
+            y={36}
+            text={data[i]}
+            style={dataLabelStyles(i)}
+            dy={i * 36}
+          />
+        ))}
+      </g>
       {square ? (
         <ScaledSquare
           colorScale={chart.colorScale}
           reference={reference}
           sides={data}
           size={minDimension}
-          x={0}
-          y={0}
+          x={(computedWidth - minDimension) / 2}
+          y={data.length * 36 + 10}
         />
       ) : (
         <ScaledCircle
           colorScale={chart.colorScale}
           cx={computedWidth / 2}
-          cy={computedHeight / 2}
+          cy={data.length * 36 + 10 + chartHeight / 2}
           groupSpacing={computedGroupSpacing}
           reference={reference}
           radii={data}
           size={minDimension / 2 - computedGroupSpacing}
           theme={(theme as unknown) as VictoryThemeDefinitionLatest}
-          height={computedHeight}
+          height={chartHeight}
           width={computedWidth}
           labels={() => ''}
         />
       )}
+      <VictoryLabel
+        capHeight={0}
+        lineHeight={0}
+        x={(computedWidth - minDimension) / 2}
+        y={computedHeight - 24}
+        text={reference.data[0]}
+        style={referenceLabelStyles(0)}
+      />
+      <VictoryLabel
+        capHeight={0}
+        lineHeight={0}
+        x={(computedWidth - minDimension) / 2}
+        y={computedHeight}
+        text="Tanzania"
+        style={referenceLabelStyles(1)}
+      />
     </CustomContainer>
   );
 }
