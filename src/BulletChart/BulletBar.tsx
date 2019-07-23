ecf1273fe5d67dde975836bbc4ac7f85096bb6d1
Clemence Kyara <clemence@codeforafrica.org>
2019-07-23 08:47:38 +0300

    Fix reference and add comparison mode
     + Reference is the comparative value and not part of qualitative
     scales/measure,
     + Add comparison mode that works in both mobile and desktop modes

--- src/BulletChart/BulletBar.tsx
+++ src/BulletChart/BulletBar.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 
-import { Rect, VictoryLabel, VictoryStyleObject } from 'victory';
+import { Rect, VictoryLabel, VictoryStyleInterface } from 'victory';
 
 import { ReferenceProps } from '../ReferableChart';
 
 interface Props<T> {
   barWidth?: number;
-  // Feature measure
-  data: number;
+  // Feature measure & qualitative measure
+  data: number[];
   height: number;
-  // Qualitative measure
-  reference: ReferenceProps<number>;
-  style?: VictoryStyleObject;
   // Comparative measure
-  target?: number;
+  reference: ReferenceProps<number>;
+  style?: VictoryStyleInterface;
   width: number;
+  x: number;
   y: number;
 }
 
@@ -22,13 +21,22 @@ function BulletBar({
   barWidth = 5,
   data,
   reference,
-  style,
-  target,
+  style = {},
   width,
+  x,
   y
 }: Props<number>) {
-  const computedData = (width * data) / reference.data[0];
-  const computedTarget = target ? (width * target) / reference.data[0] : 0;
+  // Assume numbers are percentage & no total provided & both data[0] and
+  // reference are <= 100
+  let total = 100;
+  if (data.length > 1) {
+    [, total] = data; // second element is total
+  } else if (data[0] > 100 || reference.data[0] > 100) {
+    total = data[0] + Math.max(data[0], reference.data[0]);
+  }
+  const qualitativeMeasure = total - data[0];
+  const featuredMeasure = (width * data[0]) / total;
+  const comparativeMeasure = (width * reference.data[0]) / total;
   return (
     <React.Fragment>
       {/* Qualitative measure */}
@@ -37,47 +45,43 @@ function BulletBar({
           capHeight={0}
           lineHeight={0}
           textAnchor="end"
-          x={width}
+          x={x + width}
           y={y - 2 * barWidth}
-          text={reference.labels[0]}
-          style={
-            reference.style && (reference.style.labels as React.CSSProperties)
-          }
+          text={qualitativeMeasure}
+          style={style.labels as React.CSSProperties}
         />
       )}
       <Rect
-        x={0}
+        x={x}
         y={y - barWidth}
         width={width}
         height={barWidth}
-        style={reference.style && (reference.style.data as React.CSSProperties)}
+        style={style.labels}
       />
       {/* Feature measure */}
       <VictoryLabel
         capHeight={0}
         lineHeight={0}
-        x={0}
+        x={x}
         y={y - 2 * barWidth}
-        text={data}
-        style={style as React.CSSProperties}
+        text={data[0]}
+        style={style.labels as React.CSSProperties}
       />
       <Rect
-        x={0}
+        x={x}
         y={y - barWidth}
-        width={computedData}
+        width={featuredMeasure}
         height={barWidth}
-        style={style}
+        style={style.data}
       />
       {/* Comparative measure */}
-      {computedTarget && (
-        <Rect
-          x={computedTarget}
-          y={y - barWidth}
-          width={barWidth}
-          height={barWidth}
-          style={{ fill: 'black' }}
-        />
-      )}
+      <Rect
+        x={x + comparativeMeasure}
+        y={y - barWidth}
+        width={barWidth}
+        height={barWidth}
+        style={reference.style && (reference.style.data as React.CSSProperties)}
+      />
     </React.Fragment>
   );
 }
--- src/BulletChart/index.tsx
+++ src/BulletChart/index.tsx
@@ -18,12 +18,18 @@ interface Props
     VictoryMultiLabeableProps,
     ReferableChartProps<number> {}
 
+/**
+ * By default, Bullet chart assumes data is a percentage **if** only one value
+ * provided and its less than 100. Otherwise, it will assume data provided is
+ * half i.e. convert `[data]`  to `[data, data]`
+ */
 function BulletChart({ data, height, reference: ref, theme, width }: Props) {
   const { bullet: chart } = (theme as unknown) as VictoryThemeDefinitionLatest;
   if (!data || !chart) {
     return null;
   }
 
+  const computedData = Array.isArray(data[0]) ? data : [data];
   const computedHeight = height || chart.height;
   const computedWidth = width || chart.width;
   const reference = Object.assign(
@@ -31,18 +37,34 @@ function BulletChart({ data, height, reference: ref, theme, width }: Props) {
     { style: chart.reference },
     toReferenceProps(ref)
   );
+  const computedStyle = Object.assign({}, chart.style);
+
   return (
     <CustomContainer width={width} height={height}>
-      {data.map((d, i) => (
+      {computedData.map((d, i) => (
         <g>
           <BulletBar
             data={d}
             height={computedHeight}
-            target={52}
             reference={reference}
-            style={{ fill: chart.colorScale[i % chart.colorScale.length] }}
-            width={computedWidth}
-            y={computedHeight}
+            style={Object.assign({}, computedStyle, {
+              data: { fill: chart.colorScale[i % chart.colorScale.length] }
+            })}
+            width={
+              computedWidth < 600 || computedData.length < 2
+                ? computedWidth
+                : (computedWidth - 20) / 2
+            }
+            x={
+              computedWidth < 600 || computedData.length < 2
+                ? 0
+                : (i * computedWidth) / 2
+            }
+            y={
+              computedWidth < 600 || computedData.length < 2
+                ? computedHeight - i * 50
+                : computedHeight
+            }
           />
         </g>
       ))}
