18df10065e30a62b049640d34ce8c75a76368beb
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-07-31 13:44:07 +0300

    Fix dy

--- src/WrapLabel/wrapSVGText.ts
+++ src/WrapLabel/wrapSVGText.ts
@@ -2,7 +2,6 @@ export default (textElement: SVGTextElement, text: string, width: number) => {
   const words = text.split(/\s+/).reverse();
   let word = words.pop();
   let line: any = [];
-  let lineNumber = 1;
   const lineHeight = 14;
   const x = textElement.getAttribute('x');
   const textAnchor = textElement.getAttribute('text-anchor');
@@ -33,10 +32,7 @@ export default (textElement: SVGTextElement, text: string, width: number) => {
       tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
       tspan.textContent = word;
       tspan.setAttribute('text-anchor', textAnchor || 'middle');
-      tspan.setAttribute(
-        'dy',
-        (lineNumber * lineHeight + parseFloat(dy || '0')).toString()
-      );
+      tspan.setAttribute('dy', lineHeight.toString());
 
       if (x) {
         tspan.setAttribute('x', x);
@@ -45,8 +41,6 @@ export default (textElement: SVGTextElement, text: string, width: number) => {
         tspan.setAttribute('dx', dx);
       }
       textElement.appendChild(tspan);
-
-      lineNumber += 1;
     }
     word = words.pop();
   }
