81c51899500b79e75a850e4c4bdf2eed6573d3fa
Clemence Kyara <clemence@codeforafrica.org>
2019-08-23 16:30:03 +0300

    Initial data sorting support

--- src/PieChart/DonutLabel.js
+++ src/PieChart/DonutLabel.js
@@ -5,13 +5,37 @@ import { VictoryTooltip } from 'victory';
 
 import PieLabel from './PieLabel';
 
+const sortData = (data, sortKey) => {
+  let sortedData = data;
+  if (sortKey) {
+    const sort = (a, b, whenLarge = 1, whenSmall = -1) => {
+      if (a.y > b.y) {
+        return whenLarge;
+      }
+      if (b.y > a.y) {
+        return whenSmall;
+      }
+      return 0;
+    };
+    const sortDescending = (a, b) => sort(a, b, -1, 1);
+    sortedData =
+      sortKey[0] === '-' ? data.sort(sortDescending) : data.sort(sort);
+  }
+  return sortedData;
+};
+
 function DonutLabel(props) {
-  const { colorScale, x, y } = props;
+  const { colorScale, data, sortKey, x, y } = props;
+  const sortedData = sortData(data, sortKey);
+
   return (
-    <g>
-      <PieLabel colorScale={colorScale} text="boom" x={x} y={y} />
-      <VictoryTooltip labelComponent={<PieLabel />} {...props} />
-    </g>
+    <PieLabel
+      colorScale={colorScale}
+      style={{ textAnchor: 'middle' }}
+      text={sortedData[0].label}
+      x={x}
+      y={y}
+    />
   );
 }
 
@@ -19,12 +43,15 @@ DonutLabel.defaultEvents = VictoryTooltip.defaultEvents;
 
 DonutLabel.propTypes = {
   colorScale: PropTypes.arrayOf(PropTypes.shape({})),
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  sortKey: PropTypes.oneOf(['value', '-value']),
   x: PropTypes.number,
   y: PropTypes.number
 };
 
 DonutLabel.defaultProps = {
   colorScale: undefined,
+  sortKey: undefined,
   x: undefined,
   y: undefined
 };
