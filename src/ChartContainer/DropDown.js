fe112113b63e1c3bec77b5833256e793048ec8a7
Clemence Kyara <clemence@codeforafrica.org>
2019-09-02 12:39:56 +0300

    Add reusable DropDown popper

---
+++ src/ChartContainer/DropDown.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/styles';
+import { ClickAwayListener, Fade, Paper, Popper } from '@material-ui/core';
+
+const useStyles = makeStyles(({ breakpoints }) => ({
+  root: {
+    width: '100%',
+    marginTop: '1rem',
+    [breakpoints.up('sm')]: {
+      width: '20rem'
+    }
+  },
+  paper: {}
+}));
+
+function DropDown({ children, onClose, open, transition, ...props }) {
+  const classes = useStyles();
+
+  return (
+    <Popper open={open} className={classes.root} transition {...props}>
+      {({ TransitionProps }) => (
+        <Fade {...TransitionProps} {...transition}>
+          <Paper className={classes.paper}>
+            <ClickAwayListener onClickAway={onClose}>
+              {children}
+            </ClickAwayListener>
+          </Paper>
+        </Fade>
+      )}
+    </Popper>
+  );
+}
+
+DropDown.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  transition: PropTypes.shape({})
+};
+
+DropDown.defaultProps = {
+  transition: {
+    timeout: 350
+  }
+};
+
+export default DropDown;
