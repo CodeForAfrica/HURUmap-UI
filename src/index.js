95f427199d39837ad976d3d45e39aedb1067f383
Clemence Kyara <clemence@codeforafrica.org>
2019-09-02 12:50:24 +0300

    Update stories

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
+export { default as createTheme } from './styles/createTheme';
+export { default as A } from './A';
 export { default as MapIt } from './MapIt';
 export { default as BarChart } from './BarChart';
+export { default as BlockLoader } from './BlockLoader';
 export { default as BulletChart } from './BulletChart';
+export { default as ChartContainer } from './ChartContainer';
+export { default as ComparisonBarChart } from './ComparisonBarChart';
+export { default as ContentLoader } from './ContentLoader';
+export { default as InfoPanel } from './ChartContainer/InfoPanel';
 export { default as LineChart } from './LineChart';
-export { default as PieChart } from './PieChart';
-export { default as NumberVisuals } from './NumberVisuals';
 export {
   default as NestedProportionalAreaChart
 } from './NestedProportionalAreaChart';
-export { default as createTheme } from './styles/createTheme';
-export { default as ComparisonBarChart } from './ComparisonBarChart';
-export { default as ChartContainer } from './ChartContainer';
-export { default as ShareDropDown } from './ChartContainer/ShareDropDown';
-export { default as InfoDropDown } from './ChartContainer/InfoDropDown';
-export { default as ContentLoader } from './ContentLoader';
-export { default as BlockLoader } from './BlockLoader';
+export { default as NumberVisuals } from './NumberVisuals';
+export { default as PieChart } from './PieChart';
+export { default as SharePanel } from './ChartContainer/SharePanel';
 export { default as TypographyLoader } from './TypographyLoader';
-export { default as A } from './A';
-
--- stories/container.stories.js
+++ stories/container.stories.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, number, select, text } from '@storybook/addon-knobs';
+import {
+  boolean,
+  number,
+  select,
+  text,
+  withKnobs
+} from '@storybook/addon-knobs';
 
 import Grid from '@material-ui/core/Grid';
 
-import { BarChart, ChartContainer, ShareDropDown, InfoDropDown } from '../src';
+import { Typography } from '@material-ui/core';
+import { BarChart, ChartContainer } from '../src';
 import { CenterDecorator } from './common';
 
 const rand = () => Number((Math.random() * 100).toFixed(1));
 
-storiesOf('HURUmap UI|ChartContainer', module)
+storiesOf('HURUmap UI|ChartContainers/ChartContainer', module)
   .addDecorator(CenterDecorator)
   .addDecorator(withKnobs)
   .add('Default', () =>
@@ -37,11 +44,158 @@ storiesOf('HURUmap UI|ChartContainer', module)
             <ChartContainer
               loading={boolean('loading', true)}
               title={text('title', 'Lorem ipsum dolor sit amet.')}
-              subtitle={text('Subtitle', 'Praesent at dignissim est. Integer porta consectetur ante, ut congue erat.')}
+              subtitle={text(
+                'Subtitle',
+                'Praesent at dignissim est. Integer porta consectetur ante, ut congue erat.'
+              )}
               sourceUrl={text('sourceUrl', 'http://dev.dominion.africa')}
               content={{
                 height: 338
               }}
+            >
+              <BarChart
+                horizontal={boolean('horizontal', false)}
+                width={500}
+                height={300}
+                data={Array(number('data', 100))
+                  .fill(null)
+                  .map((_, index) => ({
+                    x: `${index}-${index}`,
+                    y: rand()
+                  }))}
+                parts={{
+                  axis: {
+                    dependent: {
+                      style: {
+                        axis: {
+                          display: 'block'
+                        },
+                        grid: {
+                          display: 'block'
+                        },
+                        tickLabels: {
+                          display: 'block'
+                        }
+                      },
+                      tickValues: [10, 50, 90],
+                      tickFormat: ['10%', '50%', '90%']
+                    }
+                  }
+                }}
+              />
+            </ChartContainer>
+          </Grid>
+        </Grid>
+      );
+    })
+  )
+  .add('Hidden Drop Downs', () =>
+    React.createElement(() => {
+      return (
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          style={{ background: 'whitesmoke', height: '100%' }}
+        >
+          <Grid
+            item
+            xs={select(
+              'xs',
+              ['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              12
+            )}
+            md={select(
+              'md',
+              ['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              6
+            )}
+          >
+            <ChartContainer
+              infoComponent={null}
+              content={{
+                height: 338
+              }}
+              loading={boolean('loading', true)}
+              shareComponent={null}
+              title={text('title', 'Lorem ipsum dolor sit amet.')}
+              subtitle={text(
+                'Subtitle',
+                'Praesent at dignissim est. Integer porta consectetur ante, ut congue erat.'
+              )}
+            >
+              <BarChart
+                horizontal={boolean('horizontal', false)}
+                width={500}
+                height={300}
+                data={Array(number('data', 100))
+                  .fill(null)
+                  .map((_, index) => ({
+                    x: `${index}-${index}`,
+                    y: rand()
+                  }))}
+                parts={{
+                  axis: {
+                    dependent: {
+                      style: {
+                        axis: {
+                          display: 'block'
+                        },
+                        grid: {
+                          display: 'block'
+                        },
+                        tickLabels: {
+                          display: 'block'
+                        }
+                      },
+                      tickValues: [10, 50, 90],
+                      tickFormat: ['10%', '50%', '90%']
+                    }
+                  }
+                }}
+              />
+            </ChartContainer>
+          </Grid>
+        </Grid>
+      );
+    })
+  )
+  .add('Custom Drop Downs', () =>
+    React.createElement(() => {
+      return (
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          style={{ background: 'whitesmoke', height: '100%' }}
+        >
+          <Grid
+            item
+            xs={select(
+              'xs',
+              ['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              12
+            )}
+            md={select(
+              'md',
+              ['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+              6
+            )}
+          >
+            <ChartContainer
+              infoComponent={
+                <Typography variant="body2">Custom Info Component</Typography>
+              }
+              content={{
+                height: 338
+              }}
+              loading={boolean('loading', true)}
+              shareComponent={null}
+              title={text('title', 'Lorem ipsum dolor sit amet.')}
+              subtitle={text(
+                'Subtitle',
+                'Praesent at dignissim est. Integer porta consectetur ante, ut congue erat.'
+              )}
             >
               <BarChart
                 horizontal={boolean('horizontal', false)}
