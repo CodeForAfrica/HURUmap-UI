3e0256f0fc5e8c26b0cffa6caddf671668bfa06f
Clemence Kyara <clemence@codeforafrica.org>
2019-09-09 13:16:01 +0300

    Use share dialog similar to sensors.AFRICA

--- src/ChartContainer/ShareDropDown.js
+++ src/ChartContainer/ShareDropDown.js
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/styles';
-import { Button, Grid, Link, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton
+} from 'react-share';
 
 import DropDown from './DropDown';
 
 const useStyles = makeStyles(() => ({
   root: {},
-  source: {
+  social: {},
+  title: {
     padding: '20px',
-    width: '100%',
+    textAlign: 'center',
+    textTransform: 'uppercase',
     borderBottom: '0.0625rem solid #c4c4c4'
   },
   explore: {
@@ -20,19 +30,27 @@ const useStyles = makeStyles(() => ({
   dropDownPaper: {}
 }));
 
-function InfoPanel({
+function SharePanel({
   anchorEl,
   children,
+  email,
+  facebook,
   onClose,
-  onExploreData,
   open: openProp,
-  sourceLink,
-  sourceTitle,
+  shareIcon,
+  twitter,
+  title,
   ...props
 }) {
   const classes = useStyles(props);
   const open = typeof openProps === 'undefined' ? anchorEl !== null : openProp;
-
+  const currentUrl = window.location.href;
+  const { url: twitterShareUrlProp, ...twitterProps } = twitter || {};
+  const twitterShareUrl = twitter && (twitterShareUrlProp || currentUrl);
+  const { url: facebookShareUrlProp, ...facebookProps } = twitter || {};
+  const facebookShareUrl = facebook && (facebookShareUrlProp || currentUrl);
+  const { url: emailShareUrlProp, ...emailProps } = email || {};
+  const emailShareUrl = email && (emailShareUrlProp || currentUrl);
   return (
     <DropDown
       anchorEl={anchorEl}
@@ -41,42 +59,79 @@ function InfoPanel({
       classes={{ root: classes.dropDownRoot, paper: classes.dropDownPaper }}
     >
       <Grid className={classes.root} container justify="center" {...props}>
-        <Typography className={classes.source}>
-          {'Sources: '}
-          <Link href={sourceLink} target="_blank" rel="noopener noreferrer">
-            {sourceLink || sourceTitle}
-          </Link>
-        </Typography>
-        <Button
-          variant="outlined"
-          onClick={onExploreData}
-          className={classes.explore}
+        <Grid item xs={12}>
+          <Typography className={classes.title} variant="h2" component="h1">
+            {title}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          className={classes.social}
+          container
+          justify="center"
         >
-          {children}
-        </Button>
+          {twitter && (
+            <TwitterShareButton url={twitterShareUrl} {...twitterProps}>
+              <TwitterIcon {...shareIcon} />
+            </TwitterShareButton>
+          )}
+          {facebook && (
+            <FacebookShareButton url={facebookShareUrl} {...facebookProps}>
+              <FacebookIcon {...shareIcon} />
+            </FacebookShareButton>
+          )}
+          {email && (
+            <EmailShareButton url={emailShareUrl} {...emailProps}>
+              <EmailIcon {...shareIcon} />
+            </EmailShareButton>
+          )}
+        </Grid>
       </Grid>
     </DropDown>
   );
 }
 
-InfoPanel.propTypes = {
+SharePanel.propTypes = {
   anchorEl: PropTypes.shape({}).isRequired,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  email: PropTypes.shape({
+    subject: PropTypes.string,
+    body: PropTypes.string,
+    separator: PropTypes.string
+  }),
+  facebook: PropTypes.shape({
+    url: PropTypes.string,
+    quote: PropTypes.string,
+    hashtag: PropTypes.string
+  }),
   forwardedRef: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool,
-  onExploreData: PropTypes.func,
-  sourceLink: PropTypes.string.isRequired,
-  sourceTitle: PropTypes.string
+  shareIcon: PropTypes.shape({
+    round: PropTypes.bool,
+    size: PropTypes.number
+  }),
+  twitter: PropTypes.shape({
+    url: PropTypes.string,
+    title: PropTypes.string,
+    hashtags: PropTypes.string
+  }),
+  title: PropTypes.string
 };
 
-InfoPanel.defaultProps = {
-  onExploreData: undefined,
+SharePanel.defaultProps = {
   open: undefined,
-  sourceTitle: undefined
+  email: null,
+  facebook: null,
+  shareIcon: {
+    round: false
+  },
+  title: 'Share',
+  twitter: null
 };
 
-export default InfoPanel;
+export default SharePanel;
