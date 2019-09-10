3b50c46f3f75728a24dfefbbcf377617612ebce3
Clemence Kyara <clemence@codeforafrica.org>
2019-09-10 15:27:29 +0300

    Improve ShareDropDown design

--- src/ChartContainer/ShareDropDown.js
+++ src/ChartContainer/ShareDropDown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { DialogContent, DialogTitle, Grid, TextField } from '@material-ui/core';
 import {
   EmailIcon,
   EmailShareButton,
@@ -17,11 +17,15 @@ import DropDown from './DropDown';
 const useStyles = makeStyles(() => ({
   root: {},
   social: {},
-  title: {
-    padding: '20px',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    borderBottom: '0.0625rem solid #c4c4c4'
+  socialIcon: {
+    padding: '0 0.5rem 1.5rem 0.5rem'
+  },
+  title: {},
+  url: {
+    width: '100%'
+  },
+  urlInput: {
+    fontFamily: 'monospace'
   },
   dropDownRoot: {},
   dropDownPaper: {}
@@ -37,17 +41,19 @@ function ShareDropDown({
   shareIcon,
   twitter,
   title,
+  url: urlProp,
   ...props
 }) {
   const classes = useStyles(props);
   const open = typeof openProps === 'undefined' ? anchorEl !== null : openProp;
-  const currentUrl = window.location.href;
+  const url = urlProp || window.location.href;
   const { url: twitterShareUrlProp, ...twitterProps } = twitter || {};
-  const twitterShareUrl = twitter && (twitterShareUrlProp || currentUrl);
+  const twitterShareUrl = twitter && (twitterShareUrlProp || url);
   const { url: facebookShareUrlProp, ...facebookProps } = twitter || {};
-  const facebookShareUrl = facebook && (facebookShareUrlProp || currentUrl);
+  const facebookShareUrl = facebook && (facebookShareUrlProp || url);
   const { url: emailShareUrlProp, ...emailProps } = email || {};
-  const emailShareUrl = email && (emailShareUrlProp || currentUrl);
+  const emailShareUrl = email && (emailShareUrlProp || url);
+
   return (
     <DropDown
       anchorEl={anchorEl}
@@ -56,34 +62,57 @@ function ShareDropDown({
       classes={{ root: classes.dropDownRoot, paper: classes.dropDownPaper }}
     >
       <Grid className={classes.root} container justify="center" {...props}>
-        <Grid item xs={12}>
-          <Typography className={classes.title} variant="h2" component="h1">
-            {title}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          className={classes.social}
-          container
-          justify="center"
-        >
-          {twitter && (
-            <TwitterShareButton url={twitterShareUrl} {...twitterProps}>
-              <TwitterIcon {...shareIcon} />
-            </TwitterShareButton>
-          )}
-          {facebook && (
-            <FacebookShareButton url={facebookShareUrl} {...facebookProps}>
-              <FacebookIcon {...shareIcon} />
-            </FacebookShareButton>
-          )}
-          {email && (
-            <EmailShareButton url={emailShareUrl} {...emailProps}>
-              <EmailIcon {...shareIcon} />
-            </EmailShareButton>
-          )}
-        </Grid>
+        <DialogTitle className={classes.title}>{title}</DialogTitle>
+        <DialogContent className={classes.social}>
+          <Grid container>
+            <Grid
+              item
+              xs={12}
+              className={classes.social}
+              container
+              justify="center"
+            >
+              {twitter && (
+                <TwitterShareButton
+                  url={twitterShareUrl}
+                  {...twitterProps}
+                  className={classes.socialIcon}
+                >
+                  <TwitterIcon {...shareIcon} />
+                </TwitterShareButton>
+              )}
+              {facebook && (
+                <FacebookShareButton
+                  url={facebookShareUrl}
+                  {...facebookProps}
+                  className={classes.socialIcon}
+                >
+                  <FacebookIcon {...shareIcon} />
+                </FacebookShareButton>
+              )}
+              {email && (
+                <EmailShareButton
+                  url={emailShareUrl}
+                  {...emailProps}
+                  className={classes.socialIcon}
+                >
+                  <EmailIcon {...shareIcon} />
+                </EmailShareButton>
+              )}
+            </Grid>
+            {url && (
+              <TextField
+                defaultValue={url}
+                className={classes.url}
+                InputProps={{
+                  readOnly: true,
+                  classes: { root: classes.urlInput }
+                }}
+                variant="outlined"
+              />
+            )}
+          </Grid>
+        </DialogContent>
       </Grid>
     </DropDown>
   );
@@ -117,7 +146,8 @@ ShareDropDown.propTypes = {
     title: PropTypes.string,
     hashtags: PropTypes.string
   }),
-  title: PropTypes.string
+  title: PropTypes.string,
+  url: PropTypes.string
 };
 
 ShareDropDown.defaultProps = {
@@ -125,10 +155,12 @@ ShareDropDown.defaultProps = {
   email: null,
   facebook: null,
   shareIcon: {
-    round: false
+    round: false,
+    size: 40
   },
   title: 'Share',
-  twitter: null
+  twitter: null,
+  url: null
 };
 
 export default ShareDropDown;
