69cce363ea63045671d4ff1b48b866c37f9f770e
Clemence Kyara <clemence@codeforafrica.org>
2019-11-12 17:04:01 +0300

    Remove Material UI Styles dependency

--- src/core/InsightContainer/Insight.js
+++ src/core/InsightContainer/Insight.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { makeStyles, Button, Grid, Box } from '@material-ui/core';
+import { Button, Grid, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import BlockLoader from '../BlockLoader';
 import TypographyLoader from '../TypographyLoader';
--- stories/common.js
+++ stories/common.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-
 import React from 'react';
+
 import { Grid } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import Theme from './Theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from './theme';
 
 export const CenterDecorator = storyFn => (
   <Grid
@@ -12,6 +13,6 @@ export const CenterDecorator = storyFn => (
     alignItems="flex-start"
     style={{ width: '100%', height: 'auto', overflow: 'hidden' }}
   >
-    <ThemeProvider theme={Theme}>{storyFn()}</ThemeProvider>
+    <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
   </Grid>
 );
