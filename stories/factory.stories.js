79e02bf44e1ad87609e096364b6e88bbcbb6639a
Clemence Kyara <clemence@codeforafrica.org>
2019-12-05 12:58:37 +0300

    Update stories

--- stories/factory.stories.js
+++ stories/factory.stories.js
@@ -22,6 +22,8 @@ storiesOf('HURUmap UI|Charts Factory/ChartFactory', module)
     const horizontal = boolean('horizontal');
     const data = Array(number('data', 3)).fill(null);
     const unit = text('unit', 'u');
+    const aggregate = select('aggregate', [':percent', ''], '');
+    const customUnit = aggregate === ':percent' ? '%' : '';
     const subtitle = text('subtitle', 'Subtitle');
     const description = text('description', 'Description');
     const props = object('props', {
@@ -39,17 +41,17 @@ storiesOf('HURUmap UI|Charts Factory/ChartFactory', module)
           typeProps: {
             horizontal
           },
+          aggregate,
           unit,
           subtitle,
           description,
-          statistic
+          statistic,
+          customUnit
         }}
         data={data.map((_, index) => {
           const y = rand();
           const x = `${index} Dummy Data`;
           return {
-            name: type === 'pie' ? x : undefined,
-            tooltip: `${index} Dummy Data`,
             x,
             y
           };
