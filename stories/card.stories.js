160b70601b5c9d0cabe7d0e54ee785c0964c90b9
Clemence Kyara <clemence@codeforafrica.org>
2020-01-27 16:00:55 +0300

    Update stories [delivers #170921456]

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -64,6 +64,8 @@ storiesOf('HURUmap UI|Components/Card', module)
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`
             }
           };
+    const shareEndPoint = text('Share EndPoint', '/api/share');
+
     return (
       <ApolloProvider
         client={
@@ -80,6 +82,7 @@ storiesOf('HURUmap UI|Components/Card', module)
             geoId={geoId}
             definition={definition}
             flourishURL=""
+            shareEndPoint={shareEndPoint}
           />
         </div>
       </ApolloProvider>
