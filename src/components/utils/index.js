585d5f8ae0ee0687338adcbce807dd531545798b
Clemence Kyara <clemence@codeforafrica.org>
2020-01-27 15:42:29 +0300

    Add shareEndPoint as arg to methods as well as default value

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,14 +1,14 @@
-export const uploadImage = (id, dataUrl) =>
-  // TODO(kilemensi): We'll continue uploading images to takwimu.africa -> s3
-  // until we have this functionality in WP
-  fetch('https://takwimu.africa/api/twitter_view/', {
+const DEFAULT_SHARE_ENDPOINT = '/api/share';
+
+export const uploadImage = (id, dataUrl, endPoint) =>
+  fetch(`${endPoint || DEFAULT_SHARE_ENDPOINT}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       id,
-      image: dataUrl
+      dataUrl
     })
   }).then(res => {
     if (res.status === 200) {
@@ -18,11 +18,11 @@ export const uploadImage = (id, dataUrl) =>
     return false;
   });
 
-export const shareIndicator = (id, e, dataUrl) => {
-  uploadImage(id, dataUrl).then(success => {
+export const shareIndicator = (id, endPoint, e, dataUrl) => {
+  uploadImage(id, dataUrl, endPoint).then(success => {
     if (success) {
       const url = new URL(window.location);
-      url.searchParams.set('indicator', id);
+      url.searchParams.set('indicatorId', id);
       window.open(`https://twitter.com/intent/tweet?url=${escape(url.href)}`);
     }
   });
