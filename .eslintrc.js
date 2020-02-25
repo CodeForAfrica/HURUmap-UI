2490fa99d78927fa64ec5c8f0e0b261b72909c2b
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-02-25 05:48:34 -0330

    Clean .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,10 +38,8 @@ module.exports = {
         alias: {
           '@hurumap/cms': './packages/cms/src',
           '@hurumap/charts': './packages/charts/src',
-          '@hurumap/factory': './packages/factory/src',
           '@hurumap/core': './packages/core/src',
-          '@hurumap/config': './packages/config/src',
-          '@hurumap/mapit': './packages/mapit/src'
+          '@hurumap/config': './packages/config/src'
         }
       }
     }
