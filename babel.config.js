669ba98a69c0f785035dc1dc586dd66bb689fa00
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-02-28 07:58:40 -0330

    Configure production build

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-env'],
-  plugins: [
-    'inline-react-svg',
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@hurumap-ui/content': './packages/content/src',
-          '@hurumap-ui/charts': './packages/charts/src',
-          '@hurumap-ui/core': './packages/core/src'
-        }
-      }
-    ]
-  ]
+  plugins: ['inline-react-svg'],
+  env: {
+    development: {
+      plugins: [
+        [
+          'module-resolver',
+          {
+            alias: {
+              '@hurumap-ui/content': './packages/content/src',
+              '@hurumap-ui/charts': './packages/charts/src',
+              '@hurumap-ui/core': './packages/core/src'
+            }
+          }
+        ]
+      ]
+    }
+  }
 };
