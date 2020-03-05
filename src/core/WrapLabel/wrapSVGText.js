18e5ea31c06f6bd5f335be44d208b8975e0a7435
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-05 09:25:54 -0330

    Split computation of max label dimmension (#118)

--- src/core/WrapLabel/wrapSVGText.js
+++ src/core/WrapLabel/wrapSVGText.js
@@ -1,15 +1,79 @@
-export default (textElement, text, width, onMaxDimmension, horizontal) => {
+export function computeMaxLabelDimmension({ initialWidth, horizontal, texts }) {
   let maxDimmension = 0;
 
+  const lineHeight = '14';
+
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+  svg.setAttribute('opacity', '0');
+  document.body.appendChild(svg);
+  svg.appendChild(textEl);
+
+  const text =
+    texts.length > 1
+      ? texts.reduce((a, b) => (a.length > b.length ? a : b), '')
+      : texts[0] || '';
+
+  let line = [];
   const words = text.split(/\s+/).reverse();
   let word = words.pop();
-  let line = [];
-  const lineHeight = 14;
-  const x = textElement.getAttribute('x');
+
+  let tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
+  tspan.setAttribute('text-anchor', 'middle');
+  textEl.appendChild(tspan);
+
+  while (word) {
+    line.push(word);
+    tspan.textContent = line.join(' ');
+    let { width, height } = tspan.getBoundingClientRect();
+    if (width > initialWidth) {
+      line.pop();
+      tspan.textContent = line.join(' ');
+      line = [word];
+
+      ({ width, height } = tspan.getBoundingClientRect());
+      if (horizontal) {
+        maxDimmension += height;
+      } else if (width > maxDimmension) {
+        maxDimmension = width;
+      } else {
+        // Nothing
+      }
+
+      tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
+      tspan.setAttribute('text-anchor', 'middle');
+      tspan.setAttribute('dy', lineHeight);
+      tspan.textContent = word;
+    } else if (horizontal) {
+      maxDimmension += height;
+    } else if (width > maxDimmension) {
+      maxDimmension = width;
+    } else {
+      // Nothing
+    }
+
+    word = words.pop();
+  }
+
+  document.body.removeChild(svg);
+
+  return maxDimmension;
+}
+
+export default (textElement, text, initialWidth) => {
+  if (!textElement) {
+    return;
+  }
+
+  const words = text.split(/\s+/).reverse();
   const textAnchor = textElement.getAttribute('text-anchor');
+  const x = textElement.getAttribute('x');
+  const lineHeight = '14';
   const dy = 0;
   const dx = 0;
 
+  let line = [];
+  let word = words.pop();
   let tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
   tspan.setAttribute('text-anchor', textAnchor || 'middle');
 
@@ -23,26 +87,20 @@ export default (textElement, text, width, onMaxDimmension, horizontal) => {
     tspan.setAttribute('dx', dx);
   }
   textElement.appendChild(tspan);
+
   while (word) {
     line.push(word);
     tspan.textContent = line.join(' ');
-    if (tspan.getBoundingClientRect().width > width) {
+
+    if (tspan.getBoundingClientRect().width > initialWidth) {
       line.pop();
       tspan.textContent = line.join(' ');
       line = [word];
 
-      if (horizontal) {
-        maxDimmension += tspan.getBoundingClientRect().height;
-      } else if (tspan.getBoundingClientRect().width > maxDimmension) {
-        maxDimmension = tspan.getBoundingClientRect().width;
-      } else {
-        // Nothing
-      }
-
       tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
-      tspan.textContent = word;
       tspan.setAttribute('text-anchor', textAnchor || 'middle');
-      tspan.setAttribute('dy', lineHeight.toString());
+      tspan.setAttribute('dy', lineHeight);
+      tspan.textContent = word;
 
       if (x) {
         tspan.setAttribute('x', x);
@@ -51,16 +109,7 @@ export default (textElement, text, width, onMaxDimmension, horizontal) => {
         tspan.setAttribute('dx', dx);
       }
       textElement.appendChild(tspan);
-    } else if (horizontal) {
-      maxDimmension += tspan.getBoundingClientRect().height;
-    } else if (tspan.getBoundingClientRect().width > maxDimmension) {
-      maxDimmension = tspan.getBoundingClientRect().width;
-    } else {
-      // Nothing
     }
-
     word = words.pop();
   }
-
-  onMaxDimmension(maxDimmension);
 };
