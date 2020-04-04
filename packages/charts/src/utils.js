467d803027bc513907bad027545d557950dc45bb
Clemence Kyara <clemence@codeforafrica.org>
2020-04-04 21:37:17 +0300

    Add utility function to handle legend data

--- packages/charts/src/utils.js
+++ packages/charts/src/utils.js
@@ -1,3 +1,32 @@
+/**
+ * `groupedData` supplied to any of Bar or Line chart is in the following form:
+ * ```
+ * [
+ *  [group0bar0, group1bar0, group2bar0, ...etc],
+ *  [group0bar1, group1bar1, group2bar1, ...etc],
+ *  [group0bar2, group1bar2, group2bar2, ...etc],
+ *  ...etc
+ * ]
+ * ```
+ * The required legend data, name and (optional) description, is found in
+ * `bar0`, `bar1`, `bar2`, variables.
+ */
+export function extractLegendData(groupedData) {
+  if (
+    !groupedData ||
+    !groupedData.length ||
+    !groupedData[0].length ||
+    !groupedData[0][0].name
+  ) {
+    return undefined;
+  }
+  if (groupedData.length === 1) {
+    return groupedData;
+  }
+  // Pick first element from each row of data
+  return groupedData.map(gD => gD[0]);
+}
+
 export function getLegendProps(
   { height, width },
   initialLegendProps,
