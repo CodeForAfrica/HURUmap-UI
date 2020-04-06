2bf42493a10cabd37cc3028b8aee0457c8fd8600
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-04-06 12:44:14 -0230

    Fix theme overiding all defaults

--- packages/charts/src/styles/createVictoryTheme.js
+++ packages/charts/src/styles/createVictoryTheme.js
@@ -28,18 +28,42 @@ export default function createVictoryTheme(chartOptions) {
       },
       line: {
         offset: 100,
-        height: 300
+        height: 300,
+        legend: {
+          align: 'bottom',
+          orientation: 'horizontal',
+          size: 50
+        }
       },
       bar: {
         barWidth: 25,
         domainPadding: { x: [25, 25] },
         height: 300,
-        offset: 50
+        offset: 50,
+        legend: {
+          align: 'bottom',
+          orientation: 'horizontal',
+          size: 50
+        }
       },
       pie: {
         height: 250,
         padding: 0,
-        width: 450
+        width: 450,
+        donut: true,
+        donutHighlightIndex: 1,
+        // Default VictoryTheme.material font size is 12
+        // see: https://github.com/FormidableLabs/victory/blob/master/packages/victory-core/src/victory-theme/material.js
+        donutHighlightStyle: { fontWeight: 'bold', fontSize: 18 },
+        donutLabelRadiusRatio: 0.75,
+        donutRatio: 0.6,
+        emphasisCoefficient: 0.15,
+        groupSpacing: 4,
+        legend: {
+          align: 'right',
+          orientation: 'vertical',
+          size: 100
+        }
       },
       tooltip: {
         flyoutStyle: {
@@ -51,6 +75,32 @@ export default function createVictoryTheme(chartOptions) {
         style: {
           flyout: {
             fill: '#fff'
+          }
+        }
+      },
+      bullet: {
+        barWidth: 5,
+        offset: { x: 20, y: 50 },
+        style: {
+          data: {
+            fill: '#d8d8d8'
+          },
+          labels: {
+            fill: '#9b9b9b'
+          }
+        }
+      },
+      comparisonBar: {
+        barHeight: 5,
+        referenceStyle: {
+          labels: { fill: '#9b9b9b' },
+          data: { fill: '#9b9b9b', fontWeight: 'bold' }
+        },
+
+        style: {
+          data: {
+            fontSize: '24px',
+            fontWeight: 'bold'
           }
         }
       }
@@ -94,80 +144,18 @@ export default function createVictoryTheme(chartOptions) {
       fontSize: 18
     }
   };
-  // Customize chart bar props
-  chart.bar = {
-    legend: {
-      align: 'bottom',
-      orientation: 'horizontal',
-      size: 50
-    },
-    ...chart.bar
-  };
   // Customize chart bullet props off of chart group props
-  chart.bullet = {
-    barWidth: 5,
-    offset: { x: 20, y: 50 },
-    style: {
-      data: {
-        fill: '#d8d8d8'
-      },
-      labels: {
-        fill: '#9b9b9b'
-      }
-    },
-    ...chart.group,
-    ...chart.bullet
-  };
+  chart.bullet = _.merge(chart.bullet, chart.group);
   // Customize chart comparisonBar props off of chart group props
-  chart.comparisonBar = {
-    barHeight: 5,
-    referenceStyle: {
-      labels: { fill: '#9b9b9b' },
-      data: { fill: '#9b9b9b', fontWeight: 'bold' }
-    },
-    ...chart.group,
-    style: {
-      data: {
-        fontSize: '24px',
-        fontWeight: 'bold'
-      }
-    },
-    ...chart.comparisonBar
-  };
+  chart.comparisonBar = _.merge(chart.comparisonBar, chart.group);
   // Customize chart line props off of chart group props
-  chart.line = {
-    colorScale: chart.group.colorScale,
-    legend: {
-      align: 'bottom',
-      orientation: 'horizontal',
-      size: 50
-    },
-    ...chart.line
-  };
-  // Customize chart pie props
-  chart.pie = {
-    donut: true,
-    donutHighlightIndex: 1,
-    // Default VictoryTheme.material font size is 12
-    // see: https://github.com/FormidableLabs/victory/blob/master/packages/victory-core/src/victory-theme/material.js
-    donutHighlightStyle: { fontWeight: 'bold', fontSize: 18 },
-    donutLabelRadiusRatio: 0.75,
-    donutRatio: 0.6,
-    emphasisCoefficient: 0.15,
-    groupSpacing: 4,
-    legend: {
-      align: 'right',
-      orientation: 'vertical',
-      size: 100
-    },
-    ...chart.pie
-  };
+  chart.line.colorScale = chart.line.colorScale || chart.group.colorScale;
   // Customize chart proportionalArea props off of chart area props
-  chart.proportionalArea = {
-    reference: defaultReference,
-    ...chart.area,
-    ...chart.proportionalArea
-  };
+  chart.proportionalArea = _.merge(
+    chart.proportionalArea,
+    { reference: defaultReference },
+    chart.area
+  );
   // Use pie chart colorScale prop if proportionalArea doesn't have one
   if (!chart.proportionalArea.colorScale && chart.pie) {
     chart.proportionalArea.colorScale = chart.pie.colorScale;
