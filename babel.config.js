50b1c0811cb81e02a39524d6a0ccccf92226de3e
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-04-06 11:49:04 -0230

    Resolve modules from production and development (#133)

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,16 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-env'],
-  plugins: ['inline-react-svg'],
-  env: {
-    development: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            alias: {
-              '@hurumap-ui/content': './packages/content/src',
-              '@hurumap-ui/charts': './packages/charts/src',
-              '@hurumap-ui/core': './packages/core/src'
-            }
-          }
-        ]
-      ]
-    }
-  }
+  plugins: [
+    'inline-react-svg',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@hurumap-ui/content': './packages/content/src',
+          '@hurumap-ui/charts': './packages/charts/src',
+          '@hurumap-ui/core': './packages/core/src'
+        }
+      }
+    ]
+  ]
 };
